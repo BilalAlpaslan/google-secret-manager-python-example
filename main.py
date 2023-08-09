ba1dad11480c3290d7c900b69ae686510199a6ae
bilal.alpaslan <bilal.alpaslan@glomil.com>
2023-08-09 21:57:23 +0300

    .env

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ from utils import SecretManager
 
 secret = SecretManager("neon-radius-312707")
 
-print(secret.get_secret('DENEME_SECRET','prod'))
-print(secret.get_secret('DENEME_SECRET','dev'))
+print(secret.get_secret('DENEME_SECRET'))
+
+print(secret.get_secret('DENEME_SECRET', 'prod'))
+print(secret.get_secret('DENEME_SECRET', 'dev'))
--- utils.py
+++ utils.py
@@ -1,11 +1,19 @@
+import os
 from google.cloud import secretmanager_v1
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 class SecretManager:
     def __init__(self, project_id):
         self.client = secretmanager_v1.SecretManagerServiceClient()
         self.project_id = project_id
+        self.enviroment = os.getenv("ENVIRONMENT", "dev")
 
-    def get_secret(self, secret_name, secret_version="latest"):
+    def get_secret(self, secret_name, secret_version=None):
+        secret_version = secret_version or self.enviroment
+
         request = secretmanager_v1.AccessSecretVersionRequest(
             name="projects/{}/secrets/{}/versions/{}".format(
                 self.project_id, secret_name, secret_version
